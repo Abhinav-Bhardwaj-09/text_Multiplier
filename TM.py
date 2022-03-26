5675ebcce2362eeb3b89f88935f002ce678887f5
abhinavbhardwaj <abhinavbhardwaj68@gmail.com>
2022-03-26 22:01:03 +0530

    Required code.

--- TM.py
+++ TM.py
@@ -4,3 +4,13 @@
     * Date: 26/03/22
     * Time: 21:56
     """
+
+import pyautogui as pg
+import time
+
+text = "Text to be typped multiple times"
+
+time.sleep(5)               # Time to place cursor on the desired location
+for i in range(10):
+    pg.write(text)
+    pg.press('enter')
